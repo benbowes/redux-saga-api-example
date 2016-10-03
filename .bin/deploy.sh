778db7c1304b72167724e15ba62bc32fe05491c2
Ben Bowes <bb@benbowes.com>
2016-10-03 20:25:29 +1100

    Add deploy to gh-pages script

---
+++ .bin/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if there are uncommited changes. Exit with an error if there are.
+if [[ `git diff-index HEAD` != "" ]];
+then echo "ERROR! You have uncommited changes..." && exit 1;
+else echo "Deploy in progress...";
+fi
+
+# Build JS into dist/app.js
+npm run dist
+
+# Copy other files into dist/
+cp -r css index.html dist
+
+# git add new dist/ assets
+git add -A
+
+# Commit dist/ changes
+git commit -m "Deploy to gh-pages..."
+
+# Push commited changes to master
+git push origin master
+
+# Push dist folder to gh-pages branch
+git subtree push --prefix dist origin gh-pages
