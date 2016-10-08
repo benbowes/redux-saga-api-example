05f25f7d4078440faa600a9c2755e230f4db8837
Ben Bowes <ben.bowes@rea-group.com>
2016-10-08 12:43:00 +1100

    Add Github PR embed code

--- app/components/Image__test__.js
+++ app/components/Image__test__.js
@@ -10,6 +10,9 @@ const image = {
   images: {
     fixed_width: {
       url: 'http://some-image.url'
+    },
+    original: {
+      url: 'http://some-image.url/giphy.gif'
     }
   }
 };
@@ -20,9 +23,27 @@ describe('<Image />', () => {
     renderer.render( <Image image={image} /> );
     const ReactComponent = renderer.getRenderOutput();
 
-    expect( ReactComponent.type ).toEqual('a');
-    expect( ReactComponent.props.href ).toEqual('http://some-giphy-permalink.url');
-    expect(ReactComponent.props.children.type).toEqual(PreloadFadeInImage);
-    expect(ReactComponent.props.children.props.imgSauce).toEqual('http://some-image.url');
+    expect( ReactComponent.type ).toEqual('div');
+    expect( ReactComponent.props.children.length ).toEqual(2);
+
+    const imageContainer = ReactComponent.props.children[0];
+    expect( imageContainer.type ).toEqual('a');
+    expect( imageContainer.props.href ).toEqual('http://some-giphy-permalink.url');
+
+    const preloadFadeInImage = imageContainer.props.children;
+    expect( preloadFadeInImage.type).toEqual(PreloadFadeInImage);
+    expect( preloadFadeInImage.props.imgSauce ).toEqual('http://some-image.url');
+
+    const githubEmbedInput = ReactComponent.props.children[1].props.children[1];
+    expect( githubEmbedInput.type).toEqual('input');
+    expect( githubEmbedInput.props.defaultValue).toEqual('<img src="http://some-image.url/giphy.gif" width="100%" />');
+
+    const githubEmbedLabel = ReactComponent.props.children[1].props.children[0];
+    expect( githubEmbedLabel ).toEqual('Github embed code');
+  });
+
+  xit('Should select all input text when input receives focus', () => {
+    // ReactTestUtils.Simulate.click(node)
+    // console.log(window.getSelection().toString());
   });
 });
