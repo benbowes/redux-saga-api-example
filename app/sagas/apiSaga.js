8eaf80c52d93848b64ae101c99ae0e171b07db0d
Ben Bowes <bb@benbowes.com>
2016-10-10 03:00:18 +1100

    Alter saga imports to be *

--- app/sagas/apiSaga.js
+++ app/sagas/apiSaga.js
@@ -2,12 +2,7 @@ import { takeEvery } from 'redux-saga';
 import { put, call } from 'redux-saga/effects';
 import { fetchJsonWrapper } from './helpers/fetchJsonWrapper';
 import getSearchTermQuery from '../helpers/getSearchTermQuery';
-import {
-  RECEIVE_SEARCH_DATA,
-  REQUEST_SEARCH_DATA,
-  RECEIVE_SEARCH_DATA_FAILED,
-  REQUEST_SHOW_MORE
-} from '../constants/actionTypes';
+import * as actionTypes from '../constants/actionTypes';
 
 /*
 * @description Passes the redux action's `url` through to fetchJsonWrapper().
@@ -25,7 +20,7 @@ export function* fetchData( action ) {
 
     if ( receivedData.ok ) {
       yield put({
-        type: RECEIVE_SEARCH_DATA,
+        type: actionTypes.RECEIVE_SEARCH_DATA,
         payload: {
           receivedData: receivedData,
           searchTerm: action.payload.searchTerm
@@ -38,7 +33,7 @@ export function* fetchData( action ) {
   } catch(err) {
 
     yield put({
-      type: RECEIVE_SEARCH_DATA_FAILED,
+      type: actionTypes.RECEIVE_SEARCH_DATA_FAILED,
       payload: { error: err, failedAction: action }
     });
     //console.log( 'An error happenned :/', err );
@@ -47,16 +42,12 @@ export function* fetchData( action ) {
 
 /*
 * @description Listens for when an actionTypes.REQUEST_SEARCH_DATA action is fired
-* It calls fetchData() everytime it does.
-* It passes in the redux action payload with it to the fetchData() function above.
-
-* Note: I am using `takeEvery` here as I presume multiple requests may be made concurrently.
-* i.e. In a real world example, I might need to call multiple endpoints at the same time */
+* It calls fetchData() everytime it does- passing in the redux action payload with it*/
 
 export function* listenForDataRequests() {
-  yield* takeEvery( REQUEST_SEARCH_DATA, fetchData );
+  yield* takeEvery( actionTypes.REQUEST_SEARCH_DATA, fetchData );
 }
 
 export function* listenForShowMoreRequests() {
-  yield* takeEvery( REQUEST_SHOW_MORE, fetchData );
+  yield* takeEvery( actionTypes.REQUEST_SHOW_MORE, fetchData );
 }
--- app/sagas/gifModalImagesSaga.js
+++ app/sagas/gifModalImagesSaga.js
@@ -1,24 +1,24 @@
 import { takeEvery } from 'redux-saga';
 import { put, call, take, fork, cancel } from 'redux-saga/effects';
 import { fetchImageWrapper } from './helpers/fetchImageWrapper';
-import { GIF_MODAL_IMAGE_LOADED, GIF_MODAL_REQUEST_IMAGE, GIF_MODAL_CANCEL_REQUEST_IMAGE } from '../constants/actionTypes';
+import * as actionTypes from '../constants/actionTypes';
 
 export function* requestImage( action ) {
   const fetchImageTask = yield fork( fetchImage, action.payload.requestedImage );
 
-  // Cancels the load task - if action type GIF_MODAL_CANCEL_REQUEST_IMAGE is dispatched
-  yield take( GIF_MODAL_CANCEL_REQUEST_IMAGE );
+  // Cancels the load task if action type GIF_MODAL_CANCEL_REQUEST_IMAGE is dispatched
+  yield take( actionTypes.GIF_MODAL_CANCEL_REQUEST_IMAGE );
   yield cancel( fetchImageTask );
 }
 
 export function* fetchImage( requestedImage ) {
   const loadedImage = yield call( fetchImageWrapper, requestedImage );
   yield put({
-    type: GIF_MODAL_IMAGE_LOADED,
+    type: actionTypes.GIF_MODAL_IMAGE_LOADED,
     payload: { fullImage: loadedImage }
   });
 }
 
 export function* listenForGifModalImageRequests() {
-  yield* takeEvery( GIF_MODAL_REQUEST_IMAGE, requestImage );
+  yield* takeEvery( actionTypes.GIF_MODAL_REQUEST_IMAGE, requestImage );
 }
