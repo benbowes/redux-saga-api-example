39a7108d5a621ae8e44c0ce4329ed1b5fe77f0fa
Ben Bowes <bb@benbowes.com>
2016-10-17 00:10:22 +1100

    Add postcss-modules-values

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,47 @@
 const autoprefixer = require('autoprefixer');
+const values = require('postcss-modules-values');
 const webpack = require('webpack');
 
 module.exports = {
+
   entry: './app/index.js',
+
   output: {
     path: './dist/',
     filename: './app.js'
   },
+
   module: {
     loaders: [{
       test: /\.js?$/,
       exclude: ['node_modules'],
       loader: 'babel-loader'
-    },
-    {
+    }, {
       test: /\.s?css$/,
       include: /app/,
       exclude: ['node_modules'],
       loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
     }]
   },
-  postcss: function () {
-    return {
-      plugins: [autoprefixer]
-    };
-  },
+
+  postcss: [
+    autoprefixer,
+    values
+  ],
+
   resolve: {
     modulesDirectories: [
       'app',
       'node_modules'
     ]
   },
+
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV === 'production' ? 'production' : 'development')
+        'NODE_ENV': JSON.stringify(
+          (process.env.NODE_ENV === 'production') ? 'production' : 'development'
+        )
       }
     })
 
